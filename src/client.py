a10f6d3f0a57dc406ec648445c11a10bce560a25
yanil3500 <yanil3500@gmail.com>
2017-05-23 16:15:33 -0700

    adds tests for server.py

--- src/client.py
+++ src/client.py
@@ -1,25 +1,41 @@
+# -*- coding: utf-8 -*-
 import socket
+import sys
 
 
-addr_info = socket.getaddrinfo('127.0.0.1', 5001)
+def main(words):  # pragma: no cover
+    """
+    main function
+    """
+    if words is not '':
+        print(start_client(' '.join(words)))
 
-stream_info = [attr for attr in addr_info if attr[1] == socket.SOCK_STREAM][0]
 
-client = socket.socket(*stream_info[:3])
+def start_client(msg):
+    """
+    creates the client
+    """
+    msg = msg + '§'
+    if sys.version_info.major == 2:
+        msg = msg.decode('utf8')
+    addr_info = socket.getaddrinfo('127.0.0.1', 5009)
+    stream_info = [attr for attr in addr_info if attr[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
+    client.sendall(msg.encode('utf8'))
+    flag = True
+    res = b""
+    while flag is True:
+        more = client.recv(8)
+        res += more
+        if res[-1:] == b"\xa7":
+            flag = False
+    client.close()
+    if sys.version_info.major == 2:
+        return res[:-2]
+    return res.decode('utf8')[:-1]
 
+if __name__ == '__main__':
 
-msg = "This is a long string mesage, transmitted with a size of 8!"
-
-client.connect(stream_info[-1])
-client.sendall(msg.encode('utf8'))
-
-flag = True
-res = ""
-while flag is True:
-    more = client.recv(8)
-    res += more.decode('utf8')
-    if res[-1] == "!":
-        flag = False
-print(res)
-client.close()
-
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
--- src/server.py
+++ src/server.py
@@ -1,29 +1,32 @@
+# -*- coding: utf-8 -*-
 import socket
 import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
-address = ('127.0.0.1', 5001)
+def main():  # pragma: no cover
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5009)
+    server.bind(address)
+    server.listen(1)
+    while True:
+        try:
+            connection, address = server.accept()
+            res = b""
+            flag = True
+            while flag is True:
+                more = connection.recv(8)
+                res += more
+                print(res)
+                print(res[-1:])
+                if res[-1:] == b"\xa7":
+                    flag = False
+            print("Message sent: ", res)
+            connection.sendall(res)
+            connection.close()
+        except KeyboardInterrupt:
+            server.close()
+            sys.exit()
 
-server.bind(address)
 
-
-server.listen(1)
-while True:
-    try:
-        connection, address = server.accept()
-        res = ""
-        flag = True
-        while flag is True:
-            more = connection.recv(8)       
-            res += more.decode('utf8')
-            if res[-1] == "!":
-                flag = False
-            # if len(more.decode('utf8') == 0):
-            #     flag = False
-        print(res)
-        connection.sendall(str(res).encode('utf8'))
-
-    except KeyboardInterrupt:
-        server.close()
-        sys.exit()
+if __name__=='__main__':
+    main()
