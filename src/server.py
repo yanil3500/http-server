41f307029b8987db765399945bf1a989cb82ca94
Erik <erik.end@gmail.com>
2017-05-24 18:18:52 -0700

    did most step2 stufffs

--- src/server.py
+++ src/server.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """Server for http server assignment."""
 
@@ -9,7 +8,7 @@ import sys
 def main():  # pragma: no cover
     """Main server loop. Logs data into log variable until it finds a certain character. Then returns response."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5015)
+    address = ('127.0.0.1', 5003)
     server.bind(address)
     server.listen(1)
     while True:
@@ -20,25 +19,61 @@ def main():  # pragma: no cover
             while flag is True:
                 more = connection.recv(8)
                 log += more
-                if log[-1:] == b"\xa7":
+                if log.decode('utf8').endswith('\r\n\r\n'):
                     flag = False
-            sys.stdout.write(log.decode('utf8')[:-1])
-            response = response_ok()
+            sys.stdout.write(log.decode('utf8'))
+            URI = parse_request(log.decode('utf8'))
+            response = response_ok(URI)
             connection.sendall(response)
             connection.close()
         except KeyboardInterrupt:
+            server.shutdown(socket.SHUT_WR)
             server.close()
-            sys.exit()
+            sys.exit(0)
 
-def response_ok():#  pragma: no cover
-    response = b"HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n \xc2\xa7"
+
+def response_ok(URI):  # pragma: no cover
+    response = b"HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n"
     """Returns 200 response."""
     return response
 
-def response_error():
+def response_error(error_code):
     """Returns 500 response."""
-    response = b"HTTP/1.1 500 Internal Server Error \r\nContent-Type: text/plain \r\n \xc2\xa7"
+    if error_code == 505:
+        response = "HTTP/1.1 505 HTTP Version Not Supported \r\nContent-Type: text/plain \r\n\r\n"
+    elif error_code == 501:
+        response = "HTTP/1.1 501 Method Not Implemented \r\nContent-Type: text/plain \r\n\r\n"
+    else:
+        response = "HTTP/1.1 400 Bad Request \r\nContent-Type: text/plain \r\n\r\n"
     return response
+
+
+
+def parse_request(request):
+    words = request.split()
+    if len(words) is not 5:
+        raise Exception(response_error(400))
+    if words[0] == 'GET':
+        if words[2] == "http/1.1":
+            if words[3] == "HOST:":
+                return words[1].encode('utf8')
+            else:
+                raise Exception(response_error(400))
+        else:
+            raise Exception(response_error(505))
+    else:
+        raise Exception(response_error(501))
+
 
 if __name__== '__main__':#  pragma: no cover
     main()
+
+
+
+    #"GET /index.html HTTP/1.1\r\nHost: www.example.com"
+# GET /path/to/index.html HTTP/1.0<CRLF>
+# <CRLF>
+    #message.decode('utf8').endswith(\r\n\r\n):
+        #flag = False
+    #also maybe some logic to ensure is a proper request?
+    #IDK maybe not.
--- src/test_server.py
+++ src/test_server.py
@@ -4,13 +4,13 @@ import pytest
 import sys
 
 PARAMETERS_FOR_CLIENT = [
-    ('the quick brown fox jumped over the lazy dog!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('the man, a plan, and panama!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('a!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('1234567!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('#()@#%^&!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n '),
-    ('®!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n ')
+    ('the quick brown fox jumped over the lazy dog!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('the man, a plan, and panama!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('a!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('1234567!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('#()@#%^&!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n'),
+    ('®!', 'HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n\r\n')
 ]
 
 
@@ -24,5 +24,5 @@ def test_start_client(message, result):
 def test_error():
     """Test if the error function returns the correct status response."""
     from server import response_error
-    assert response_error() == b"HTTP/1.1 500 Internal Server Error \r\nContent-Type: text/plain \r\n \xc2\xa7"
+    assert response_error() == b"HTTP/1.1 500 Internal Server Error \r\nContent-Type: text/plain \r\n\r\n"
 
